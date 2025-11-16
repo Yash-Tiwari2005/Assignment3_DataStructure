c9b3d14c952cfa1b5c2b727e2cd1e03db26ee09d
Yash-Tiwari2005 <yash2005tiwari@gmail.com>
2025-11-16 12:04:12 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,20 @@
+/*
+ENCS205 - Data Structures
+Assignment 03: Implementation of Trees and Graph Algorithms for Campus Navigation and Data Management
+Language: Java (single-file implementation)
+
+Deliverable: ENCS205 Java implementation covering:
+- Building ADT
+- Binary Search Tree (BST) with insert/search/inorder/preorder/postorder
+- AVL Tree (with LL, RR, LR, RL rotations) and height comparison
+- Graph representation: adjacency list and adjacency matrix
+- BFS and DFS traversals
+- Dijkstra's shortest path
+- Kruskal's minimum spanning tree (using Union-Find)
+- Expression tree evaluation (simple arithmetic)
+
+Compile: javac ENCS205_Assignment03_Campus_Navigation_Planner.java
+Run: java ENCS205_Assignment03_Campus_Navigation_Planner
+
+Note: Adapt and modularize into multiple files if required by your submission format.
+*/
